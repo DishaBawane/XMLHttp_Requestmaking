b7a5d5b84f2e1c29ab5a3ea2cf905c6ad56ab46f
amol bawane <dishakhode@gmail.com>
2021-06-30 09:30:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const firstReq = new XMLHttpRequest();
+firstReq.addEventListener('load', function() {
+	console.log('IT WORKED!!!');
+	console.log(firstReq);
+	const data = JSON.parse(this.responseText);
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+});
+firstReq.addEventListener('error', () => {
+	console.log('ERROR!!!!!!');
+});
+firstReq.open('GET', 'https://swapi.co/api/planets/');
+firstReq.send();
+console.log('Request Sent!');
